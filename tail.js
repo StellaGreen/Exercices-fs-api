7dc194b1594cfac0a223cfa933fefeebe949bb7c
StellaGreen <vickysoler5@gmail.com>
2021-03-26 14:54:25 +0100

    add answer

--- tail.js
+++ tail.js
@@ -1,10 +1,52 @@
 const fs = require('fs')
 
-const txt = fs.readFileSync('file.txt', 'utf-8')
-const tab = txt.split('\n')
-//const count = tab.length
-const rv = tab.reverse()
+//check command line
+if (process.argv.length < 3 || process.argv.length > 5) {
+    console.log('Usage : node tail.js file.txt')
+    process.exit(1)
+}
 
+//if have just 3 arguments :
+if(process.argv.length === 3){
 
-console.log(rv)
+    //check if file exist
+    if (!fs.existsSync(process.argv[2])) {
+    console.log(`Sorry ${process.argv[2]} doesn't exist`)
+    process.exit(1)
+    }
+    //check if the file is a file
+    const stats = fs.statSync(process.argv[2])
+    if (!stats.isFile(process.argv[2])) {
+    console.log(`${process.argv[2]} is not a file`)
+    process.exit(1)
+    }
+    const txt = fs.readFileSync(process.argv[2], 'utf-8') //read file
+    const tab = txt.split('\n') // array of line
+    const rv = tab.reverse() // reverse the array
+    const choose = rv.slice(0, 10)
+    const rv2 = choose.reverse()
+    const joinfinal = rv2.join('\n')
+    console.log(joinfinal)
+}
 
+//if have 5 arguments :
+if(process.argv[2] === '-n'){
+    //check if file exist
+    if (!fs.existsSync(process.argv[4])) {
+    console.log(`Sorry ${process.argv[4]} doesn't exist`)
+    process.exit(1)
+    }
+    //check if the file is a file
+    const stats = fs.statSync(process.argv[4])
+    if (!stats.isFile(process.argv[4])) {
+    console.log(`${process.argv[4]} is not a file`)
+    process.exit(1)
+    }
+    const txt = fs.readFileSync(process.argv[4], 'utf-8') //read file
+    const tab = txt.split('\n') // array of line
+    const rv = tab.reverse() // reverse the array
+    const choosesp = rv.slice(0, process.argv[3])
+    const rv2v2 = choosesp.reverse()
+    const joinfinal2 = rv2v2.join('\n')
+    console.log(joinfinal2)
+}
