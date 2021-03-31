43ee41564df7714d16c9e0b93ddd6ca099312f64
StellaGreen <vickysoler5@gmail.com>
2021-03-31 11:37:03 +0200

    add check

--- tail.js
+++ tail.js
@@ -30,6 +30,33 @@ console.log(joinfinal) // result
 console.log(choose.length)
 }
 
+//TODO : 4 ARGV
+if(process.argv.length === 4){
+    if(process.argv[2] !== '-n'){
+        console.log('Usage : node tail.js -n NUMBER file.txt')
+        process.exit(1)
+    }
+ //check if file exist
+if (!fs.existsSync(process.argv[3])) {
+    console.log(`Sorry ${process.argv[3]} doesn't exist-`)
+    process.exit(1)
+}
+//check if the file is a file
+const stats = fs.statSync(process.argv[3])
+if (!stats.isFile(process.argv[3])) {
+    console.log(`${process.argv[3]} is not a file`)
+    process.exit(1)
+}
+const txt = fs.readFileSync(process.argv[3], 'utf-8') //read file
+const tab = txt.split('\n') // array of line
+const rv = tab.reverse() // reverse the array
+const choose = rv.slice(0, 10) // choose to line 1 at 10
+const rv2 = choose.reverse() // reverse the lines chooses
+const joinfinal = rv2.join('\n') // array to string of lines
+console.log(joinfinal) // result
+console.log(choose.length)
+}
+
 //if the command have 5 arguments :
 
 //check argument is valid
@@ -38,15 +65,12 @@ if(process.argv[2] !== '-n'){
     console.log('Usage : node tail.js -n NUMBER file.txt')
     process.exit(1)
 }
-}
 if(process.argv[2] === '-n'){
-
 //check if file exist
 if (!fs.existsSync(process.argv[4])) {
-console.log(`Sorry ${process.argv[4]} doesn't exist`)
+console.log(`Sorry ${process.argv[4]} doesn't exist--`)
 process.exit(1)
 }
-
 //check if the number is a number
 if(isNaN(Number(process.argv[3]))){
     console.log('Usage : node tail.js -n NUMBER file.txt')
@@ -66,4 +90,5 @@ const rv2v2 = choosesp.reverse() // reverse lines chooses
 const joinfinal2 = rv2v2.join('\n') // array to sting of lines
 console.log(joinfinal2) // result
 console.log(choosesp.length)
+}
 }
